e92706a41a89c7729173a2e047c53e0931423b7c
Ryan95Z <Ryan95F@gmail.com>
2017-12-02 13:28:46 +0000

    created inital test route

--- mscms/urls.py
+++ mscms/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from core.views import Test
+
 urlpatterns = [
+	url(r'^$', Test.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
