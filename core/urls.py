9c7bcd8b4c104bc490682e6cf545488e2f3d510c
Ryan95Z <Ryan95F@gmail.com>
2017-12-10 18:02:45 +0000

    documented urls

--- core/urls.py
+++ core/urls.py
@@ -3,15 +3,18 @@ from django.contrib.auth.decorators import login_required
 from core.views import *
 
 urlpatterns = [
+    # login and logout routes
     url(r'^login/', LoginView.as_view(), name='login'),
     url(r'^logout/', LogoutView.as_view(), name='logout'),
+
+    # urls for accessing admin parts
     url(r'^all/$', login_required(UserListView.as_view()), name='all_users'),
     url(r'^all/edit/(?P<pk>[0-9]+)/$', login_required(
         AdminUpdateUserPermissions.as_view()), name='edit_user'),
 
+    # generic user profile urls
     url(r'^profile/(?P<slug>[\w.@+-]+)/$',
         login_required(UserSettingsView.as_view()), name='user_settings'),
-
     url(r'^profile/(?P<slug>[\w.@+-]+)/details/$',
         login_required(UserUpdateDetailsView.as_view()), name='user_details')
 ]
