cf39961aecaaf9e411f54e6fc9f0d190ac75415d
Ryan95Z <Ryan95F@gmail.com>
2017-12-12 21:14:55 +0000

    tidied up admin_views_test.py

--- core/tests/views/admin_views_tests.py
+++ core/tests/views/admin_views_tests.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse
 from core.tests.common_test_utils import LoggedInTestCase
 
 
@@ -9,6 +9,7 @@ class UserListViewTest(LoggedInTestCase):
 
     def setUp(self):
         super(UserListViewTest, self).setUp()
+        self.url = reverse('all_users')
 
     def test_get_user_list_view(self):
         """
@@ -19,7 +20,7 @@ class UserListViewTest(LoggedInTestCase):
         session['username'] = "admin"
         session.save()
         login = self.client.login(username="admin", password="password")
-        response = self.client.get(reverse_lazy('all_users'))
+        response = self.client.get(self.url)
         self.assertEquals(response.status_code, 200)
 
     def test_get_user_list_view_with_incorrect_access(self):
@@ -28,16 +29,16 @@ class UserListViewTest(LoggedInTestCase):
         access rights, then we are redirected to dashboard.
         """
         self.client.force_login(self.user)
-        response = self.client.get(reverse_lazy('all_users'))
+        response = self.client.get(self.url)
         self.assertEquals(response.status_code, 302)
-        self.assertEquals(response.url, reverse_lazy('dashboard'))
+        self.assertEquals(response.url, reverse('dashboard'))
 
     def test_get_user_list_view_not_logged_in(self):
         """
         Test to check that if we are a non-logged in user,
         then we are redirected to the login.
         """
-        response = self.client.get(reverse_lazy('all_users'))
+        next_url = reverse('login') + ("?next=" + self.url)
+        response = self.client.get(reverse('all_users'))
         self.assertEquals(response.status_code, 302)
-        self.assertEquals(response.url,
-                          reverse_lazy('login') + "?next=/users/all/")
+        self.assertEquals(response.url, next_url)
