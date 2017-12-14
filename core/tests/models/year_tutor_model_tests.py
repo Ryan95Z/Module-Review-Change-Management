722e9c3f37ac033352c184e0eeecd2f1fce0a72f
Ryan95Z <Ryan95F@gmail.com>
2017-12-14 15:50:07 +0000

    created inital test for year tutor

---
+++ core/tests/models/year_tutor_model_tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+from core.models import YearTutor, UserManager
+
+
+class YearTutorTests(TestCase):
+    def setUp(self):
+        super(YearTutorTests, self).setUp()
+
