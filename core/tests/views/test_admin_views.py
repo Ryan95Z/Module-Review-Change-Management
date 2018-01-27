ea792a22ef80781ff47e53081b2fc16ca6fbc90e
Ryan95Z <Ryan95F@gmail.com>
2018-01-27 20:14:40 +0000

    added unit test for AdminYearTutorListView class

--- core/tests/views/test_admin_views.py
+++ core/tests/views/test_admin_views.py
@@ -39,7 +39,7 @@ class UserListViewTest(LoggedInTestCase):
         then we are redirected to the login.
         """
         next_url = reverse('login') + ("?next=" + self.url)
-        response = self.client.get(reverse('all_users'))
+        response = self.client.get(self.url)
         self.assertEquals(response.status_code, 302)
         self.assertEquals(response.url, next_url)
 
@@ -104,4 +104,36 @@ class TestAdminNewUserView(LoggedInTestCase):
         self.client.login(username="admin", password="password")
         response = self.client.post(self.url, data)
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.url, reverse('new_user'))
+
+
+class TestAdminYearTutorListView(LoggedInTestCase):
+
+    def setUp(self):
+        super(TestAdminYearTutorListView, self).setUp()
+        self.url = reverse('all_year_tutors')
+
+    def test_get_tutor_list_view(self):
+        """
+        """
+        session = self.client.session
+        session['username'] = "admin"
+        session.save()
+        login = self.client.login(username="admin", password="password")
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_tutor_list_view_with_incorrect_access(self):
+        """
+        """
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url, reverse('dashboard'))
+
+    def test_get_tutor_list_view_not_logged_in(self):
+        """
+        """
+        next_url = reverse('login') + ("?next=" + self.url)
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url, next_url)
