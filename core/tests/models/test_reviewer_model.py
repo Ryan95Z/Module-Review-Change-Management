6c38d87888ba6c1e7a53026496872a9bf9ba1716
Iwan Francis <iwanfrancis@gmail.com>
2018-02-01 13:51:18 +0000

    Reviewer tests now utilize the module model correctly

--- core/tests/models/test_reviewer_model.py
+++ core/tests/models/test_reviewer_model.py
@@ -1,6 +1,6 @@
 from django.db import IntegrityError
 from django.test import TestCase
-from core.models import (Reviewer, ReviewerManager, User, UserManager)
+from core.models import (Reviewer, ReviewerManager, User, UserManager, Module)
 
 class ReviewerManagerTests(TestCase):
     """
@@ -15,19 +15,43 @@ class ReviewerManagerTests(TestCase):
         user_manager = UserManager()
         user_manager.model = User
         self.user = user_manager.create_user(
-            username="johndoe",
-            first_name="John",
-            last_name="Doe",
-            email="doe@test.com",
+            username="test",
+            first_name="test",
+            last_name="test",
+            email="test@test.com",
             password="password"
         )
+
+        # create another test user to be used a module leader
+        # (module leader model not implemented atm)
+        self.module_leader = user_manager.create_user(
+            username="leader",
+            first_name="Module",
+            last_name="Leader",
+            email="ml@test.com",
+            password="password"
+        )
+
+        # create a test module
+        module_model = Module
+        self.module = module_model.objects.create(
+            module_code = "CM1101",
+            module_name = "Test Module",
+            module_credits = "10",
+            module_level = "1",
+            module_year = "1",
+            semester = "Autumn Semester",
+            delivery_language = "English",
+            module_leader = self.module_leader
+        )
+
 
     def test_create_new_model_instance(self):
         """
         Ensure that the manager can correctly create
         a new user model and include it in the reviewer model.
         """
-        module_code = "CM1101"
+        module = self.module
         username = "johndoe"
         first_name = "John"
         last_name = "Doe"
@@ -35,8 +59,8 @@ class ReviewerManagerTests(TestCase):
         password = "password"
 
         # create the model
-        model = self.manager.create_new_tutor(
-            module_code=module_code,
+        model = self.manager.create_new_reviewer(
+            module=module,
             username=username,
             first_name=first_name,
             last_name=last_name,
@@ -44,63 +68,64 @@ class ReviewerManagerTests(TestCase):
             password=password
         )
 
-        self.assertEquals(model.module_code, module_code)
+        self.assertEquals(model.module, module)
         self.assertEquals(model.get_reviewer_name(), "{} {}".format(first_name, last_name))
         self.assertEquals(model.get_reviewer_username(), username)
 
         user = model.reviewer_user
         self.assertEquals(user.email, email)
         # check that manager set the permission to be true
-        self.assertTrue(user.is_reviewer)
+        self.assertTrue(user.is_module_reviewer)
 
     def test_create_model_existing_user(self):
         """
         Creating a module reviewer from an existing user.
         """
-        module_code = "CM1101"
+        module = self.module
 
         # check permission is not already set
-        self.assertFalse(self.user.is_reviewer)
+        self.assertFalse(self.user.is_module_reviewer)
 
-        model = self.manager.create_reviewer(module_code, self.user)
+        model = self.manager.create_reviewer(module, self.user)
 
-        self.assertEquals(model.module_code, module_code)
+        self.assertEquals(model.module, module)
         self.assertEquals(model.get_reviewer_name(), self.user.get_full_name())
         self.assertEquals(model.get_reviewer_username(), self.user.username)
 
         # check that the permissions were updated
         user = model.reviewer_user
         self.assertEquals(user.email, self.user.email)
-        self.assertTrue(self.user.is_reviewer)
+        self.assertTrue(self.user.is_module_reviewer)
 
     def test_create_reviewer_with_no_user(self):
         """
         Test case for checking that a reviewer cannot be
         created if there is no user provided.
         """
-        module_code = "CM1101"
+        module = self.module
         with self.assertRaises(ValueError):
-            self.manager.create_tutor(module_code, None)
+            self.manager.create_reviewer(module, None)
 
-    def test_create_reviewer_with_no_module_code(self):
+    def test_create_reviewer_with_no_module(self):
         """
         Test case for checking that an invaid string
-        for tutor year will not create a model.
+        for module_code will not create a model.
         """
         with self.assertRaises(ValueError):
-            self.manager.create_tutor("", self.user)
+            self.manager.create_reviewer(None, self.user)
 
 class ReviewerTests(TestCase):
     """
     Test cases for Reviewer
     """
     def setUp(self):
-        super(ReviewerManagerTests, self).setUp()
+        super(ReviewerTests, self).setUp()
         self.model = Reviewer
 
         user_manager = UserManager()
         user_manager.model = User
 
+        # create a test user
         self.user = user_manager.create_user(
             username="johndoe",
             first_name="John",
@@ -109,20 +134,43 @@ class ReviewerTests(TestCase):
             password="password"
         )
 
+        # create another test user to be used a module leader
+        # (module leader model not implemented atm)
+        self.module_leader = user_manager.create_user(
+            username="leader",
+            first_name="Module",
+            last_name="Leader",
+            email="ml@test.com",
+            password="password"
+        )
+
+        # create a test module
+        module_model = Module
+        self.module = module_model.objects.create(
+            module_code = "CM1101",
+            module_name = "Test Module",
+            module_credits = "10",
+            module_level = "1",
+            module_year = "1",
+            semester = "Autumn Semester",
+            delivery_language = "English",
+            module_leader = self.module_leader
+        )
+
     def test_valid_reviewer(self):
         """
         Test case to check that the model
         is valid with expected data.
         """
         self.model.objects.create(
-            module_code="CM1101",
+            module=self.module,
             reviewer_user=self.user
         )
 
         reviewer = Reviewer.objects.get(pk=1)
 
-        self.assertEquals(tutor.module_code, "CM1101")
-        self.assertEquals(tutor.get_reviewer_name(), self.user.get_full_name())
-        self.assertEquals(tutor.get_reviewer_username(), self.user.username)
-        self.assertEquals(tutor.get_reviewer_id(), self.user.id)
+        self.assertEqual(reviewer.module, self.module)
+        self.assertEquals(reviewer.get_reviewer_name(), self.user.get_full_name())
+        self.assertEquals(reviewer.get_reviewer_username(), self.user.username)
+        self.assertEquals(reviewer.get_reviewer_id(), self.user.id)
 
