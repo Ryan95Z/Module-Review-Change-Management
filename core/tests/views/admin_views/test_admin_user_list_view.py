ffc093a372b2ef621b0aa34bd857ebb02e3389f1
Ryan95Z <Ryan95F@gmail.com>
2018-02-03 14:10:37 +0000

    renamed test_user_list_view to be test_admin_user_list_view

--- core/tests/views/admin_views/test_admin_user_list_view.py
+++ core/tests/views/admin_views/test_admin_user_list_view.py
@@ -3,13 +3,13 @@ from core.tests.views.admin_views.admin_test_case import AdminViewTestCase
 from core.models import User
 
 
-class UserListViewTest(AdminViewTestCase):
+class AdminUserListViewTest(AdminViewTestCase):
     """
     Test case for testing the UserListView
     """
 
     def setUp(self):
-        super(UserListViewTest, self).setUp()
+        super(AdminUserListViewTest, self).setUp()
         self.url = reverse('all_users')
 
     def test_get_user_list_view(self):
@@ -17,29 +17,18 @@ class UserListViewTest(AdminViewTestCase):
         Test to check that if we are logged in as
         a admin, then we can access the view.
         """
-        login = self.client.login(
-            username=self.admin.username,
-            password=self.admin_password
-        )
-        response = self.client.get(self.url)
-        self.assertEquals(response.status_code, 200)
+        self.run_get_view(self.url)
 
     def test_get_user_list_view_with_incorrect_access(self):
         """
         Test that if we are not a user with the correct
         access rights, then we are redirected to dashboard.
         """
-        self.client.force_login(self.user)
-        response = self.client.get(self.url)
-        self.assertEquals(response.status_code, 302)
-        self.assertEquals(response.url, reverse('dashboard'))
+        self.run_get_view_incorrect_access(self.url)
 
     def test_get_user_list_view_not_logged_in(self):
         """
         Test to check that if we are a non-logged in user,
         then we are redirected to the login.
         """
-        next_url = reverse('login') + ("?next=" + self.url)
-        response = self.client.get(self.url)
-        self.assertEquals(response.status_code, 302)
-        self.assertEquals(response.url, next_url)
+        self.run_get_view_not_logged_in(self.url)
