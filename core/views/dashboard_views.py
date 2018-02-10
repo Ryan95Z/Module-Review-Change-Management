ccba2695fd4d670bd7a4f63d789b5f321954446b
Iwan Francis <iwanfrancis@gmail.com>
2018-02-10 16:23:06 +0000

    Added "My Modules" card to dashboard

--- core/views/dashboard_views.py
+++ core/views/dashboard_views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from core.models import Module
 
 
 class DashboardView(TemplateView):
@@ -6,3 +7,10 @@ class DashboardView(TemplateView):
     Generic view to display dashboard
     """
     template_name = 'core/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_modules'] = Module.objects.filter(module_leader=self.request.user)
+        return context
+
+    
