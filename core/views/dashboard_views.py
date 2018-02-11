cb3e2e6c46dc98ce71bc9ca806d9709ccd92df8f
Iwan Francis <iwanfrancis@gmail.com>
2018-02-11 11:34:21 +0000

    Added "All modules" card to dashboard
    The cards on the dashboard are kept inside a "card-columns" div, so they
    should automatically forms into nice columns as more are added.

--- core/views/dashboard_views.py
+++ core/views/dashboard_views.py
@@ -11,6 +11,7 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['my_modules'] = Module.objects.filter(module_leader=self.request.user)
+        context['all_modules'] = Module.objects.all()
         return context
 
     
