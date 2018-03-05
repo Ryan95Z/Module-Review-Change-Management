8c2b0346b6e75282ca942ece62c73f767756ca17
Iwan Francis <iwanfrancis@gmail.com>
2018-03-05 10:32:46 +0000

    Form builder no longer crashes if there are no existing form versions

--- forms/views/admin/admin_module_description_form_structure_views.py
+++ forms/views/admin/admin_module_description_form_structure_views.py
@@ -1,6 +1,7 @@
 from django.views import View
 from django.shortcuts import render, redirect
 from django.forms import modelformset_factory, formset_factory
+from django.core.exceptions import ObjectDoesNotExist
 
 from core.models import Module
 from forms.models import ModuleDescriptionFormVersion, FormFieldEntity
@@ -43,8 +44,12 @@ class AdminModuleDescriptionFormModify(View):
         self.field_formset_object = formset_factory(FieldEntityForm, extra=0)
 
     def get(self, request, **kwargs):
-        newest_version_fields = FormFieldEntity.objects.get_most_recent_form()
-        field_formset = self.field_formset_object(request.GET or None, initial=newest_version_fields)
+        try:
+            newest_version_fields = FormFieldEntity.objects.get_most_recent_form()
+            field_formset = self.field_formset_object(request.GET or None, initial=newest_version_fields)
+        except ObjectDoesNotExist :
+            field_formset = self.field_formset_object(request.GET or None)
+
         return render(request, self.template, {'field_formset': field_formset})
 
     def post(self, request, **kwargs):
