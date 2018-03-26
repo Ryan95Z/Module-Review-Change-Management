11544d27786ff3d5a3d5b1d26e0640e158c4960c
Iwan Francis <iwanfrancis@gmail.com>
2018-03-26 17:45:34 +0100

    Irrelevant module support fields are cleared on form submission
    * If the user fills the skills or notes fields in but then sets the checkbox to false the fields will be cleared on submission.
    * Doing it this way means that if they change their mind or click the checkbox accidentally they won't have lost their data.

--- forms/static/forms/js/tracking_form.js
+++ forms/static/forms/js/tracking_form.js
@@ -1,3 +1,18 @@
+// When the form is submitted we remove any values in the support fields if 
+// the support required checkbox is not checked.
+$('#tracking-form').submit(function(event) {
+    var lab_support_required = $('#id_lab_support_required').is(':checked')
+    var tutorial_support_required = $('#id_tutorial_support_required').is(':checked')
+    if(!lab_support_required) {
+        $('#id_lab_support_skills').val('')
+        $('#id_lab_support_notes').val('')
+    }
+    if(!tutorial_support_required) {
+        $('#id_tutorial_support_skills').val('')
+        $('#id_tutorial_support_notes').val('')
+    }
+})
+
 // When the page first loads we need to check to see whether the lab/tutorial
 // support fields should be shown or collapsed.
 $(document).ready(function(){
