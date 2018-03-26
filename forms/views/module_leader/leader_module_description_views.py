13d7886d42415270be15948256845acbe4a90af9
Iwan Francis <iwanfrancis@gmail.com>
2018-03-26 15:57:03 +0100

    Module details can now be edited in MD form
    * Changes won't show until they are confirmed in the timeline. Need a way to know when something has been submitted for review

--- forms/views/module_leader/leader_module_description_views.py
+++ forms/views/module_leader/leader_module_description_views.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from django.views import View
 from core.models import Module
-from forms.forms import ModuleDescriptionForm
+from forms.forms import ModuleDetailForm, ModuleDescriptionForm
 from forms.models import ModuleDescription, ModuleDescriptionEntry, ModuleDescriptionFormVersion, FormFieldEntity
 from forms.utils.module_description import md_to_form
 
@@ -25,6 +25,9 @@ class LeaderModuleDescriptionView(View):
         form_type = kwargs.get('form_type', 'view')
         form_exists = True
         edit_form = True if form_type == 'new' else False
+
+        # Populate the ModuleDetails form
+        details_form = ModuleDetailForm(instance=module)
         
         # Retrieve the most recent description for this module, and
         # the form version it used. Convert it to form format.
@@ -54,6 +57,7 @@ class LeaderModuleDescriptionView(View):
             'edit_form': edit_form,
             'form_exists': form_exists,
             'module': module,
+            'details_form': details_form,
             'form': module_description_form,
             'new_form_version': new_form_version
         }
@@ -68,6 +72,9 @@ class LeaderModuleDescriptionView(View):
         """
         module = Module.objects.get(pk=self.kwargs.get('pk'))
 
+        # Get the module details form 
+        module_details_form = ModuleDetailForm(request.POST, instance=module)
+
         # In the future, the user may be able to update the description
         # using an older version of the form, therefore we get the form
         # version from the form so that we know what we are working with
@@ -77,8 +84,12 @@ class LeaderModuleDescriptionView(View):
 
         # If the form data is valid, we create a new ModuleDescription
         # 'parent' object, and then create ModuleDescriptionEntry objects
-        # for each of the fields in the form, associating them with the parent
-        if module_description_form.is_valid():
+        # for each of the fields in the form, associating them with the parent.
+        # We also update the module details
+        if module_description_form.is_valid() and module_details_form.is_valid():
+
+            # Save any changes to the details
+            module_details_form.save()
 
             # Remove the form version from the cleaned data, as we don't need it
             del module_description_form.cleaned_data['form_version']
@@ -96,6 +107,7 @@ class LeaderModuleDescriptionView(View):
                 'edit_form': True,
                 'form_exists': True,
                 'module': module,
+                'details_form': module_details_form,
                 'form': module_description_form,
                 'new_form_version': False
             }
