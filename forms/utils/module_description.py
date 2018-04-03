3b3ac52bf9de6f00fc8083663b0eee0e6a9433a4
Iwan Francis <iwanfrancis@gmail.com>
2018-04-03 21:51:31 +0100

    Changed ModuleDescriptionClass to Wrapper
    * Also now handles error if there is no module description when current is called

--- forms/utils/module_description.py
+++ forms/utils/module_description.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from django.core.exceptions import ObjectDoesNotExist
+
 from forms.models.module_description import *
 
 def md_to_form(queryset):
@@ -18,41 +20,49 @@ def md_to_form(queryset):
         form_data[id_for_form] = entry.string_entry
     return form_data
 
-class AbstractModuleDescriptionClass(ABC):
+class AbstractModuleDescriptionWrapper(ABC):
     """
     Abstract Module Description Class
     """
-    def __init__(self, module, module_description_master):
+    def __init__(self, module, module_description_master=False):
         self.module = module 
         self.module_description_master=module_description_master
-        self.form = FormFieldEntity.objects.get_form(module_description_master.form_version.pk)
-        self.values_queryset = ModuleDescriptionEntry.objects.get_full_description(self.module_description_master)
+
+        if self.module_description_master:
+            self.form = FormFieldEntity.objects.get_form(module_description_master.form_version.pk)
+            self.values_queryset = ModuleDescriptionEntry.objects.get_full_description(self.module_description_master)
 
     def get_data_with_labels(self):
-        data = {}
-        for field in self.form:
-            field_id = field.entity_id
-            field_label = field.entity_label
-            field_type = field.entity_type
-            if field_type in ("text-input", "text-area", "multi-choice", "radio-buttons"):
-                field_data = self.values_queryset.get(field_id=field_id).string_entry
-            elif field_type in ("check-boxes"):
-                field_data = self.values_queryset.get(field_id=field_id).boolean_entry
-            data[field_label] = field_data
-        return data
+        if self.module_description_master:
+            data = {}
+            for field in self.form:
+                field_id = field.entity_id
+                field_label = field.entity_label
+                field_type = field.entity_type
+                if field_type in ("text-input", "text-area", "multi-choice", "radio-buttons"):
+                    field_data = self.values_queryset.get(field_id=field_id).string_entry
+                elif field_type in ("check-boxes"):
+                    field_data = self.values_queryset.get(field_id=field_id).boolean_entry
+                data[field_label] = field_data
+            return data
+        else:
+            return "There is no module description for this module"
 
-class ModuleDescriptionClass(AbstractModuleDescriptionClass):
+class ModuleDescriptionWrapper(AbstractModuleDescriptionWrapper):
     """
     The module description with the given id
     """
     def __init__(self, module_description):
         module = module_description.module
-        super(ModuleDescriptionClass, self).__init__(module, module_description)
+        super(ModuleDescriptionWrapper, self).__init__(module, module_description)
 
-class CurrentModuleDescriptionClass(AbstractModuleDescriptionClass):
+class CurrentModuleDescriptionWrapper(AbstractModuleDescriptionWrapper):
     """
     The most recent Module Description for a given module
     """
     def __init__(self, module):
-        current_module_description = ModuleDescription.objects.get_most_recent(module)
-        super(CurrentModuleDescriptionClass, self).__init__(module, current_module_description)
+        try:
+            current_module_description = ModuleDescription.objects.get_most_recent(module)
+            super(CurrentModuleDescriptionWrapper, self).__init__(module, current_module_description)
+        except ObjectDoesNotExist:
+            super(CurrentModuleDescriptionWrapper, self).__init__(module)
