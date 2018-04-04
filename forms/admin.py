e73640d16628a69a28d8854c6b4d21c849ebf765
Iwan Francis <iwanfrancis@gmail.com>
2018-04-04 13:34:55 +0100

    Attempted fix for Ryan

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from forms.models import *
 
 # Register your models here.
+admin.site.register(ModuleChangeSummary)
+admin.site.register(ModuleTeaching)
+admin.site.register(ModuleSupport)
+admin.site.register(ModuleAssessment)
+admin.site.register(ModuleReassessment)
+admin.site.register(ModuleSoftware)
+
+admin.site.register(FormFieldEntity)
+admin.site.register(ModuleDescriptionFormVersion)
+admin.site.register(ModuleDescription)
+admin.site.register(ModuleDescriptionEntry)
