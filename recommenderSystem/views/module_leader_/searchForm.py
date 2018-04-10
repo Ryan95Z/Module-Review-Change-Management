085365e63984e06da41ec1a2f4963414e94e2025
waadalhajri <waad.alhjri@hotmail.com>
2018-04-10 22:13:13 +0100

    view improvement -
    convert view from function-based to class-based view

--- recommenderSystem/views/module_leader_/searchForm.py
+++ recommenderSystem/views/module_leader_/searchForm.py
@@ -8,25 +8,24 @@ from forms.forms import ModuleChangeSummaryForm, ModuleTeachingHoursForm, Module
 from recommenderSystem.forms import ModuleSoftwareSearchForm
 from forms.utils.tracking_form import *
 
-from timeline.utils.timeline.tracking_form import tracking_to_timeline, get_form_version_number
-
-
-# software search form view - waad part
-def software_search_ajax(request):
-    # Get the search term
-    search_term = request.GET.get('search_term')
-
-    items = ModuleSoftware.objects.filter(software_name__icontains=search_term)
-    # use json module to return a JSON object to the frontend of each row matching the search term
-    return HttpResponse(json.dumps({
-        'results': [
-        {
-            'software': x.software_name,
-            'module': x.module.module_name,
-            'module_id': x.module_id,
-
-            'package': x.software_packages,
-            'version': x.software_version
-        } for x in items]
-        }
-    ))
+# software search form view
+class searchBar(View):
+    def get(self, request, *args, **kwargs):
+        # Get the search term
+        search_term = request.GET.get('search_term')
+        #  __contains gets translated by Django into a SQL LIKE statement
+        items = ModuleSoftware.objects.filter(software_name__icontains=search_term)
+        # use json module to return a JSON object to the frontend of each row matching the search term
+        return HttpResponse(json.dumps({ # return any module_software where the software name containes the search term
+        # create an object that we can send to the frontend
+            'results': [
+            # dictionary with the data we need (wrap it in a python dictionary and put them into results)
+            {
+                'software': x.software_name,
+                'module': x.module.module_name,
+                'module_id': x.module_id,
+                'package': x.software_packages,
+                'version': x.software_version
+            } for x in items] # each x is one item
+            }
+        ))
