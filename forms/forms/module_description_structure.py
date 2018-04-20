dcbb84aa0fde050177abfa14e2f0305f56cbcf5c
Iwan Francis <iwanfrancis@gmail.com>
2018-04-20 18:07:35 +0100

    Extra validation/help for form builder

--- forms/forms/module_description_structure.py
+++ forms/forms/module_description_structure.py
@@ -8,3 +8,18 @@ class FieldEntityForm(forms.ModelForm):
     class Meta:
         model = FormFieldEntity
         exclude = ('entity_id','module_description_version','entity_default')
+
+    def clean(self):
+        data = super().clean()
+        field_type = data.get('entity_type')
+        choices = data.get('entity_choices').strip()
+
+        if field_type in ("multi-choice", "radio-buttons"):
+            if choices == '':
+                self.add_error('entity_choices', 'Must provide choices seperated by commas')
+            elif ',' not in choices:
+                self.add_error('entity_choices', 'Must provide at least two options, seperated by commas')
+            elif choices.startswith(',') or choices.endswith(','):
+                self.add_error('entity_choices', 'Must not start or end with a comma')
+
+        return data
